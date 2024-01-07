779dc24bccf8eb6b1e19f8560d9aea9834f5f5aa
Vasiliynemix <vasyanemix@gmail.com>
2024-01-07 16:41:53 +0300

    add migrate table

--- cmd/migrator/main.go
+++ cmd/migrator/main.go
@@ -80,5 +80,57 @@ func runMigrations(db *gorm.DB) error {
 		return err
 	}
 
+	users := getUsers(db)
+	for _, user := range users {
+		_ = addSchedule(db, user.TelegramID)
+	}
+
+	return nil
+}
+
+func getUsers(db *gorm.DB) []models.User {
+	var users []models.User
+	result := db.Find(&users)
+	if result.Error != nil {
+		fmt.Println("Failed to get users", result.Error)
+		return []models.User{}
+	}
+
+	return users
+}
+
+func addSchedule(db *gorm.DB, TelegramID int64) error {
+	newSchedule := models.Schedule{
+		TelegramID: TelegramID,
+	}
+
+	result := db.Create(&newSchedule)
+	if result.Error != nil {
+		fmt.Println("Failed to add schedule", result.Error)
+		return result.Error
+	}
+
+	newWeekEven := models.WeekEven{
+		ScheduleID: newSchedule.ID,
+	}
+
+	result = db.Create(&newWeekEven)
+	if result.Error != nil {
+		fmt.Println("Failed to add weekEven", result.Error)
+		return result.Error
+	}
+
+	newWeekOdd := models.WeekOdd{
+		ScheduleID: newSchedule.ID,
+	}
+
+	result = db.Create(&newWeekOdd)
+	if result.Error != nil {
+		fmt.Println("Failed to add weekOdd", result.Error)
+		return result.Error
+	}
+
+	fmt.Println("Schedule, weekEven and weekOdd added", TelegramID)
+
 	return nil
 }
